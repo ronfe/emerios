2b3685912d4486ed99577cff36cb45e29e640368
centsent <ivan.hui.g+d@gmail.com>
2015-12-04 00:43:47 +0800

    feat: add dataset to wheels

---
+++ wheels/dataset.py
@@ -0,0 +1,49 @@
+__author__ = 'centsent'
+from metaConfig import *
+
+def aggregate(condition):
+    pipeLine = [
+        {
+            "$match": {
+                "eventKey": condition,
+                "eventTime": {"$gte": START_DATE, "$lt": END_DATE},
+                "platform": "app",
+                "platform2": "iOS"
+            }
+        },
+        {
+            "$group": {
+                "_id": None,
+                "devices": {"$addToSet": "$device"}
+            }
+        }
+    ]
+
+    device_list = list(events.aggregate(pipeLine))[0]['devices']
+
+    pipeLine = [
+        {
+            "$match": {
+                "eventKey": condition,
+                "eventTime": {"$gte": START_DATE, "$lt": END_DATE},
+                "platform": "app",
+                "platform2": "iOS"
+            }
+        },
+        {"$group": {"_id": None, "users": {"$addToSet": "$user"}}}
+    ]
+
+    user_list = list(events.aggregate(pipeLine))[0]['users']
+
+    result_device = set.intersection(set(new_users['devices']), set(device_list))
+
+    new_user_list = []
+    for k, v in new_users.iteritems():
+        new_user_list.extend(v)
+
+    result_user = set.intersection(set(new_user_list), set(user_list))
+
+    return {
+        device: result_device,
+        user: result_user
+    }
