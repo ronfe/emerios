27b908c50c446fba3a78f9d146966f9df4624ae0
diggzhang <diggzhang@gmail.com>
2015-12-07 16:50:51 +0800

    增加练习模块以及视频模块部分数据需求

--- scripts/startMasterTop10.py
+++ scripts/startMasterTop10.py
@@ -15,30 +15,77 @@ top10MasterList = []
 for i in range(10):
     top10MasterList.append(str(top10Master[i]))
 
-def completionRate(mEvent, dEvent, top10MasterList):
-    m = dataset.aggregateTopic(mEvent, top10TopicsList)
-    mNum = len(m['user']) + len(m['device'])
-    d = dataset.aggregateTopic(dEvent, top10TopicsList)
-    dNum = len(d['user']) + len(d['device'])
-    return mNum/dNum
-
-print("学习模块总完成率：")
-print(completionRate("completeLearning", "startLearning", top10TopicsList))
-
-# 视频总观看量
-def startCompleteMaster(condition, top10TopicsList):
-    user = dataset.aggregateTopic(condition, top10TopicsList)
-    return len(user['device']) + len(user['user'])
-
-print("视频总观看量：")
-print(startFinishVideo("startVideo", top10TopicsList))
-# 视频完成总量
-print("视频完成总量：")
-print(startFinishVideo("finishVideo", top10TopicsList))
-
-
 # 练习进入总量
-# 练习模块真实总完成率
-# 完成量/真正进入量
+def startMasterTotalNumber(top10MasterList):
+    pipeLine = [
+        {"$match": {
+            "eventKey": "startMaster",
+            "eventValue.topicId": top10MasterList
+        }},
+        {"$group": {
+            "_id": None,
+            "users": {"$addToSet": "$user"}
+        }}
+    ]
+
+    return list(events.aggregateMaster(pipeLine))
+
 # 挑战失败量/挑战进入量
+def failureRetetion(top10MasterList):
+    pipeLine = [
+        {"$match": {
+            "eventKey": "completeMaster",
+            "eventValue.topicId": top10MasterList
+        }},
+        {"$group": {
+            "_id": None,
+            "users": {"$addToSet": "$user"}
+        }}
+    ]
+    completeMasterNum = len(list(events.aggregateMaster(pipeLine)))
+
+    pipeLine = [
+        {"$match": {
+            "eventKey": "startMaster",
+            "eventValue.topicId": top10MasterList
+        }},
+        {"$group": {
+            "_id": None,
+            "users": {"$addToSet": "$user"}
+        }}
+    ]
+    startMasterNum = len(list(events.aggregateMaster(pipeLine)))
+
+    return completeMasterNum/startMasterNum
+
+
+# 挑战完成率
 # 挑战成功+失败量/挑战进入量
+def successFinishRetetion(top10MasterList):
+    pipeLine = [
+        {"$match": {
+            "eventKey": {"$in": ["challengeFailure", "challengeSuccess"]},
+            "eventValue.topicId": {"$in": top10MasterList}
+        }},
+        {"$group": {
+            "_id": None,
+            "users": {"$addToSet": "$user"}
+        }}
+    ]
+    successAndFailureNum = len(list(events.aggregateMaster(pipeLine)))
+
+    pipeLine = [
+        {"$match": {
+            "eventKey": "startChallenge",
+            "eventValue.topicId": {"$in": top10MasterList}
+        }},
+        {"$group": {
+            "_id": None,
+            "users": {"$addToSet": "$user"}
+        }}
+    ]
+    finishNum = len(list(events.aggregateMaster(pipeLine)))
+
+    return successAndFailureNum/finishNum
+
+print("挑战完成率: %s")%(successFinishRetetion(top10MasterList))
