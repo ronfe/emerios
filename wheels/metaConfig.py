b3fac852bc56581feb474e1eda3696e3b8a55f58
ronfe <chihungfei@126.com>
2015-12-10 17:32:49 +0800

    [ronfe] finish task #6

--- wheels/metaConfig.py
+++ wheels/metaConfig.py
@@ -7,10 +7,14 @@ import threading
 import calendar
 
 db = MongoClient('10.8.8.111:27017')['onlinedb30']
+another_db = MongoClient('10.8.8.111:27017')['yangcong-prod25']
+db_3 = MongoClient('10.8.8.111:27017')['onions-cb']
 events = db['events']
 users = db['users']
-user_attr = db['userAttr']
+user_attr = another_db['userAttr']
 device_cache = db['deviceCache']
+topics = db_3['topics']
+hypervideos = db_3['hypervideos']
 
 START_DATE = datetime.datetime(2015,11,1)
 END_DATE = datetime.datetime(2015,11,1,1)
