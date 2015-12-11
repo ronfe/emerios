aaf575a0e457a0ad4d2a03fc064d97bb0ba2cd15
diggzhang <diggzhang@gmail.com>
2015-12-11 12:46:50 +0800

    分子分母不能为零

--- wheels/getPercent.py
+++ wheels/getPercent.py
@@ -1,4 +1,6 @@
 def getPerNum(molecule, denominator):
+    if molecule == 0 or denominator == 0:
+        return str("比值不能为零")
     m = float(molecule)
     d = float(denominator)
     num = ("%.2f")%(m/d*100)
